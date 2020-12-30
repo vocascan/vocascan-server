39e4f89a396ee2df33b855f41f1e914ec7f1ffbf
noctera <julian.jaksch@gmx.de>
2020-12-30 16:29:01 +0100

    implemented createPackage function in database

--- src/database/database.hpp
+++ src/database/database.hpp
@@ -27,18 +27,8 @@ public:
 
     bool addRole(const std::string &name, bool adminRights);
     bool checkTableEmpty(const std::string &tableName);
-    //bool checkExistingEntity(const std::string &name, const std::string &tableName, const std::string &columnName);
 
-    //void addLanguagePackage(const LanguagePackage &lngPckg);
-    //std::vector<std::string> getLanguagePackages();
-
-    //bool createDrawer(const std::string &name, int queryInterval, const std::string &lngPckgName);
-
-    //void addGroup(const std::string &name, const std::string &lngPckName);
-    //std::vector<std::string> getGroups(std::string packageName);
-
-    //bool addForeignWord(const ForeignWord &foreignWord);
-    //bool addTranslatedWord(const TranslatedWord &translatedWord);
+    bool createLanguagePackage(LanguagePackage lngPackage);
 
 private:
     std::string conn;
