40d592136bb2dce2b506e19270116dd29262096e
noctera <>
2021-01-10 11:05:09 +0100

    renamed and added new files

---
+++ tests/database-test.cpp
@@ -0,0 +1,13 @@
+/*#include <catch2/catch.hpp>
+#include "../src/database/database.hpp"
+
+TEST_CASE("Check initialization", "[initialization]")
+{
+    REQUIRE(Database database("vocascan", "vocascan", "vocascan", "127.0.0.1", 5432););
+}
+
+TEST_CASE("check database available", "[database-up]")
+{
+    Database database("vocascan", "vocascan", "vocascan", "127.0.0.1", 5432);
+    REQUIRE(database.checkDatabaseAvailable() == true);
+}*/
