be08df873be748bb84dc3bb4a66a77269096971b
noctera <julian.jaksch@gmx.de>
2021-03-19 23:16:49 +0100

    adding drawers when creating language packages

--- app/Controllers/LanguagePackageController.js
+++ app/Controllers/LanguagePackageController.js
@@ -1,4 +1,5 @@
 const { createLanguagePackage, getLanguagePackages } = require('../Services/LanguagePackageServiceProvider.js');
+const { createDrawer} = require('../Services/DrawerServiceProvider.js');
 const { parseTokenUserId } = require('../utils/index.js');
 
 async function addLanguagePackage(req, res) {
@@ -7,6 +8,23 @@ async function addLanguagePackage(req, res) {
 
   // create language Package
   const languagePackage = await createLanguagePackage(req.body, userId);
+
+  //create drawers for language package
+  const drawers = [
+    {name: "1", queryInterval: 1},
+    {name: "2", queryInterval: 2},
+    {name: "3", queryInterval: 3},
+    {name: "4", queryInterval: 5},
+    {name: "5", queryInterval: 10},
+    {name: "6", queryInterval: 30},
+    {name: "7", queryInterval: 60},
+    {name: "8", queryInterval: 90}
+  ]
+
+  //iterate over drawers and store them in the database
+  for (let drawer of drawers) {
+    createDrawer(languagePackage.id, drawer.name, drawer.queryInterval, userId);
+  }
 
   res.send(languagePackage);
 }
