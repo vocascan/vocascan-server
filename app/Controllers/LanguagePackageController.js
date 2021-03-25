6a94fe8f0601f6e3cbccba7f5c4501829dbc9611
noctera <julian.jaksch@gmx.de>
2021-03-25 22:41:37 +0100

    group output for get language package route

--- app/Controllers/LanguagePackageController.js
+++ app/Controllers/LanguagePackageController.js
@@ -1,6 +1,7 @@
 const { createLanguagePackage, getLanguagePackages } = require('../Services/LanguagePackageServiceProvider.js');
 const { createDrawer } = require('../Services/DrawerServiceProvider.js');
 const { drawers } = require('../utils/constants.js');
+const { getGroups } = require('../Services/GroupServiceProvider.js');
 const {
   getNumberOfUnresolvedVocabulary,
   getNumberOfUnactivatedVocabulary,
@@ -26,20 +27,38 @@ async function addLanguagePackage(req, res) {
 async function sendLanguagePackages(req, res) {
   // get userId from request
   const { id } = await req.user;
+  const { groups } = (await req.query) || false;
 
   // get language Package
   const languagePackages = await getLanguagePackages(id, res);
 
-  const formatted = await Promise.all(
-    languagePackages.map(async (languagePackage) => ({
-      unresolvedVocabularies: await getNumberOfUnresolvedVocabulary(languagePackage.id, id),
+  let formatted;
+  // if groups is true, return groups to every language package
+  if (groups) {
+    formatted = await Promise.all(
+      languagePackages.map(async (languagePackage) => ({
+        unresolvedVocabularies: await getNumberOfUnresolvedVocabulary(languagePackage.id, id),
 
-      // add number of unactivated vocabularies
-      unactivatedVocabularies: await getNumberOfUnactivatedVocabulary(languagePackage.id, id),
+        // add number of unactivated vocabularies
+        unactivatedVocabularies: await getNumberOfUnactivatedVocabulary(languagePackage.id, id),
 
-      ...languagePackage.toJSON(),
-    }))
-  );
+        groups: await getGroups(id, languagePackage.id, res),
+
+        ...languagePackage.toJSON(),
+      }))
+    );
+  } else {
+    formatted = await Promise.all(
+      languagePackages.map(async (languagePackage) => ({
+        unresolvedVocabularies: await getNumberOfUnresolvedVocabulary(languagePackage.id, id),
+
+        // add number of unactivated vocabularies
+        unactivatedVocabularies: await getNumberOfUnactivatedVocabulary(languagePackage.id, id),
+
+        ...languagePackage.toJSON(),
+      }))
+    );
+  }
 
   res.send(formatted);
 }
