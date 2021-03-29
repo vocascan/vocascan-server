1c41401f3245b78d2d3c41403a32db8c3862f293
noctera <julian.jaksch@gmx.de>
2021-03-29 12:06:03 +0200

    fixed some bugs

--- app/Controllers/QueryController.js
+++ app/Controllers/QueryController.js
@@ -9,21 +9,18 @@ async function sendQueryVocabulary(req, res) {
   // get userId from request
   const { id } = req.user;
   const { languagePackageId } = req.params;
-  const { limit } = req.query;
+  const { limit } = { limit: '0', ...req.query };
+  const { staged } = { staged: false, ...req.query };
+  // convert to bool
+  const isStaged = staged === 'true';
 
-  // parse vocabulary card id from response and create translations
-  const vocabulary = await getQueryVocabulary(languagePackageId, id, limit);
-
-  res.send(vocabulary);
-}
-
-async function sendUnactivatedVocabulary(req, res) {
-  // get userId from request
-  const { id } = req.user;
-  const { languagePackageId } = req.params;
-
-  // parse vocabulary card id from response and create translations
-  const vocabulary = await getUnactivatedVocabulary(languagePackageId, id);
+  let vocabulary;
+  // if staged = true return the staged vocabulary
+  if (isStaged) {
+    vocabulary = await getUnactivatedVocabulary(languagePackageId, id);
+  } else {
+    vocabulary = await getQueryVocabulary(languagePackageId, id, limit);
+  }
 
   res.send(vocabulary);
 }
@@ -32,10 +29,12 @@ async function checkVocabulary(req, res) {
   // get userId from request
   const { id } = req.user;
   const { vocabularyId } = req.params;
-  const { answer } = req.body;
+  const { answer } = req.query;
+  // convert to bool
+  const isAnswerRight = answer === 'true';
 
   // check if vocabulary card got answered right
-  if (answer === true) {
+  if (isAnswerRight) {
     handleCorrectQuery(id, vocabularyId);
     res.sendStatus(204);
   } else {
@@ -46,6 +45,5 @@ async function checkVocabulary(req, res) {
 
 module.exports = {
   sendQueryVocabulary,
-  sendUnactivatedVocabulary,
   checkVocabulary,
 };
