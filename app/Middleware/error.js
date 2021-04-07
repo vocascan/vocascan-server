b49f30d7d259cb506b9e4142f4a824f3c58fe566
Julian <julian.jaksch@gmx.de>
2021-04-07 23:37:20 +0200

    removed stack from error response

--- app/Middleware/error.js
+++ app/Middleware/error.js
@@ -24,7 +24,7 @@ const errorConverter = (err, req, res, next) => {
       statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     }
     const message = error.message || httpStatus[statusCode];
-    error = new ApiError(statusCode, message, false, err.stack);
+    error = new ApiError(statusCode, message, false);
   }
   next(error);
 };
@@ -42,7 +42,6 @@ const errorHandler = (err, req, res, next) => {
   const response = {
     code: statusCode,
     message,
-    ...{ stack: err.stack },
   };
 
   return res.status(statusCode).send(response);
--- app/utils/ApiError.js
+++ app/utils/ApiError.js
@@ -1,13 +1,8 @@
 class ApiError extends Error {
-  constructor(statusCode, message, isOperational = true, stack = '') {
+  constructor(statusCode, message, isOperational = true) {
     super(message);
     this.statusCode = statusCode;
     this.isOperational = isOperational;
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
   }
 }
 
