4616712351ef0355a293d2984b5a3819e5d25cdf
luwol03 <luwol03@gmail.com>
2021-05-10 09:01:11 +0200

    added iso-639-1 languages

--- database/seeders/0002.languages.js
+++ database/seeders/0002.languages.js
@@ -1,34 +1,24 @@
-const languages = [
-  {
-    code: 'de',
-    name: 'German',
-    nativeNames: ['Deutsch'],
-    rtl: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    code: 'en',
-    name: 'English',
-    nativeNames: ['English'],
-    rtl: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
+/**
+ * Source: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+ * Parsed with: https://gist.github.com/luwol03/9e8c635353371b1464841a114b142d63
+ */
+const iso639 = require('./data/languages-iso-639-1.json');
+
+const languages = Object.entries(iso639).map(([code, lang]) => ({
+  code,
+  name: lang.name,
+  nativeNames: lang.nativeNames.join(','),
+  rtl: lang.rtl,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}));
 
 async function up({ context: queryInterface }) {
-  await queryInterface.bulkInsert(
-    'languages',
-    languages.map((lang) => ({
-      ...lang,
-      nativeNames: lang.nativeNames.join(','),
-    }))
-  );
+  await queryInterface.bulkInsert('languages', languages);
 }
 
 async function down({ context: queryInterface }) {
-  await queryInterface.bulkDelete('languages', { name: languages.map((l) => l.code) });
+  await queryInterface.bulkDelete('languages', { code: languages.map((l) => l.code) });
 }
 
 module.exports = { up, down };
