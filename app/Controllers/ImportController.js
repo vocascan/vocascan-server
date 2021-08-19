47a9ae3b4e1c310f849b53051038e5b551b41fad
noctera <noctera.dev@gmail.com>
2021-08-19 15:14:53 +0200

    feat: updated params

--- app/Controllers/ImportController.js
+++ app/Controllers/ImportController.js
@@ -1,33 +1,32 @@
 const { storeGroupVocabulary, storeLanguagePackageVocabulary } = require('../Services/ImportServiceProvider.js');
 const catchAsync = require('../utils/catchAsync');
 
-const importGroup = catchAsync(async (req, res) => {
-  // get userId from request
-  const userId = req.user.id;
+const importGroup = catchAsync(async(req, res) => {
+    // get userId from request
+    const userId = req.user.id;
 
-  // get group with vocabs from request body
-  const { group, languagePackageId, active, activate } = req.body;
+    const { active, activate, languagePackageId } = req.query;
 
-  await storeGroupVocabulary(group, userId, languagePackageId, active, activate);
+    await storeGroupVocabulary(req.body, userId, languagePackageId, active, activate);
 
-  res.send('stored');
+    res.send('stored');
 });
 
-const importLanguagePackage = catchAsync(async (req, res) => {
-  // get userId from request
-  const userId = req.user.id;
+const importLanguagePackage = catchAsync(async(req, res) => {
+    // get userId from request
+    const userId = req.user.id;
 
-  // get group with vocabs from request body
-  const { languagePackage, active, activate } = req.body;
+    // get group with vocabs from request body
+    const { active, activate } = req.query;
 
-  await storeLanguagePackageVocabulary(languagePackage, userId, active, activate);
+    await storeLanguagePackageVocabulary(req.body, userId, active, activate);
 
-  // await storeLanguagePackageVocabulary(group);
+    // await storeLanguagePackageVocabulary(group);
 
-  res.send('stored');
+    res.send('stored');
 });
 
 module.exports = {
-  importGroup,
-  importLanguagePackage,
+    importGroup,
+    importLanguagePackage,
 };
