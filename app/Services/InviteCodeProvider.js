00ea28a4f2c387ef890459ebb77fc401e686f584
noctera <noctera.dev@gmail.com>
2021-08-26 16:07:47 +0200

    fix: updated validations

--- app/Services/InviteCodeProvider.js
+++ app/Services/InviteCodeProvider.js
@@ -5,79 +5,75 @@ const httpStatus = require('http-status');
 const { deleteKeysFromObject } = require('../utils');
 const { Sequelize } = require('sequelize');
 
-const createInviteCode = async ({ maxUses, expirationDate }) => {
-  const inviteCode = await InviteCode.create({
-    code: uuid(),
-    used: 0,
-    maxUses,
-    expirationDate,
-  });
-  return deleteKeysFromObject(['createdAt', 'updatedAt'], inviteCode.toJSON());
+const createInviteCode = async({ maxUses, expirationDate }) => {
+    const inviteCode = await InviteCode.create({
+        used: 0,
+        maxUses,
+        expirationDate,
+    });
+    return deleteKeysFromObject(['createdAt', 'updatedAt'], inviteCode.toJSON());
 };
-const validateInviteCode = async (code) => {
-  const inviteCode = await InviteCode.findOne({
-    where: {
-      code,
-    },
-  });
+const validateInviteCode = async(code) => {
+    const inviteCode = await InviteCode.findOne({
+        where: {
+            code,
+        },
+    });
 
-  if (!inviteCode) {
-    throw new ApiError(httpStatus.CONFLICT, 'Invite code does not exist');
-  }
-
-  if (inviteCode.maxUses - inviteCode.used === 0) {
-    throw new ApiError(httpStatus.CONFLICT, 'No invites left');
-  }
-
-  if (inviteCode.expirationDate >= new Date()) {
-    throw new ApiError(httpStatus.CONFLICT, 'Invite code expired');
-  }
-
-  return true;
-};
-
-const useInviteCode = async (code) => {
-  const counter = await InviteCode.update(
-    { used: Sequelize.literal('used + 1') },
-    {
-      where: {
-        code,
-      },
+    if (!inviteCode) {
+        throw new ApiError(httpStatus.CONFLICT, 'Invite code does not exist');
     }
-  );
 
-  if (counter[0] === 0) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Invite code not found');
-  }
-  return false;
+    if (inviteCode.maxUses !== null && inviteCode.maxUses - inviteCode.used === 0) {
+        throw new ApiError(httpStatus.CONFLICT, 'No invites left');
+    }
+
+    if (inviteCode.expirationDate !== null && inviteCode.expirationDate >= new Date()) {
+        throw new ApiError(httpStatus.CONFLICT, 'Invite code expired');
+    }
+
+    return true;
 };
 
-const destroyInviteCode = async (code) => {
-  const counter = await InviteCode.destroy({
-    where: {
-      code,
-    },
-  });
+const useInviteCode = async(code) => {
+    const counter = await InviteCode.update({ used: Sequelize.literal('used + 1') }, {
+        where: {
+            code,
+        },
+    });
 
-  if (counter === 0) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Invite code not found');
-  }
+    if (counter[0] === 0) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Invite code not found');
+    }
+    return false;
+};
 
-  return false;
+const destroyInviteCode = async(code) => {
+    const counter = await InviteCode.destroy({
+        where: {
+            code,
+        },
+    });
+
+    if (counter === 0) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Invite code not found');
+    }
+
+    return false;
 };
 
 async function getInviteCodes() {
-  const inviteCodes = await InviteCode.findAll({
-    attributes: ['id', 'code', 'used', 'maxUses', 'expirationDate'],
-  });
+    const inviteCodes = await InviteCode.findAll({
+        attributes: ['id', 'code', 'used', 'maxUses', 'expirationDate'],
+    });
 
-  return inviteCodes;
+    return inviteCodes;
 }
 
 module.exports = {
-  createInviteCode,
-  validateInviteCode,
-  useInviteCode,
-  destroyInviteCode,
-  getInviteCodes,
+    createInviteCode,
+    validateInviteCode,
+    useInviteCode,
+    destroyInviteCode,
+    getInviteCodes,
 };
