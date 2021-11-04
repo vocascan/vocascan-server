fd84365843ba92c6ed2942d8827db87672f1fc4b
noctera <noctera.dev@gmail.com>
2021-11-04 12:04:31 +0100

    style: fixed identation

--- docker/traefik/vocascan.config.js
+++ docker/traefik/vocascan.config.js
@@ -3,32 +3,31 @@
  * This config file lists all available options
  */
 
- module.exports = {
-    debug: false,
-  
-    server: {
-      port: 8000,
-      jwt_secret: '',
-      salt_rounds: 10,
+module.exports = {
+  debug: false,
+
+  server: {
+    port: 8000,
+    jwt_secret: '',
+    salt_rounds: 10,
+  },
+
+  database: {
+    dialect: 'postgres',
+    host: 'db',
+    port: '5432',
+    username: 'vocascan',
+    password: 'vocascan',
+    database: 'vocascan',
+  },
+
+  log: {
+    console: {
+      level: 'info',
+      colorize: true,
+      enable_sql_log: true,
+      enable_router_log: true,
+      stderr_levels: ['error'],
     },
-  
-    database: {
-      dialect: 'postgres',
-      host: 'db',
-      port: '5432',
-      username: 'vocascan',
-      password: 'vocascan',
-      database: 'vocascan',
-    },
-  
-    log: {
-      console: {
-        level: 'info',
-        colorize: true,
-        enable_sql_log: true,
-        enable_router_log: true,
-        stderr_levels: ['error'],
-      },
-    },
-  };
-  
+  },
+};
