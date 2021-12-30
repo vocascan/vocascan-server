1e71ed179245ea768423975f22e47c9f0fd6678e
luwol03 <luwol03@gmail.com>
2021-12-30 15:24:19 +0100

    added cors origin config option

--- app/config/joi.js
+++ app/config/joi.js
@@ -1,9 +1,20 @@
-const Joi = require('joi').extend((joi) => ({
-  base: joi.array(),
-  type: 'stringArray',
-  coerce: (value) => ({
-    value: value.split ? value.split(',') : value,
-  }),
-}));
+const Joi = require('joi')
+  .extend((joi) => ({
+    base: joi.array(),
+    type: 'stringArray',
+    coerce: (value) => ({
+      value: value.split ? value.split(',') : value,
+    }),
+  }))
+  .extend((joi) => ({
+    base: joi.array(),
+    type: 'keyArray',
+    coerce: (value) => {
+      if (typeof value === 'object') {
+        return { value: Object.values(value) };
+      }
+      return { value: [value] };
+    },
+  }));
 
 module.exports = Joi;
