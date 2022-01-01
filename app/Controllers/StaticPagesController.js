bb75094b9da2f055e65e9c5b9709857cb6ab62db
noctera <noctera.dev@gmail.com>
2022-01-01 18:42:40 +0100

    refactor: cleaned up code

--- app/Controllers/StaticPagesController.js
+++ app/Controllers/StaticPagesController.js
@@ -1,27 +1,44 @@
 const catchAsync = require('../utils/catchAsync');
 const path = require('path');
+const ApiError = require('../utils/ApiError.js');
+const httpStatus = require('http-status');
+
+const renderHandler = catchAsync(async (res, page) => {
+  const options = { root: path.join(__dirname, '../../') };
+
+  // check if page should be rendered from a static file or should be redirected to another one
+  if (page.type === 'file') {
+    res.sendFile(page.location, options, (err) => {
+      if (err) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Page was not found');
+      }
+    });
+  } else {
+    res.redirect(page.location);
+  }
+});
 
 const renderPage = catchAsync(async (req, res, pageValues) => {
   // check which language the user wants his page
   const language = req.query.lang;
-  const options = { root: path.join(__dirname, '../../') };
+
+  // if no language is given, render fallback page
+  if (!language) {
+    const fallbackPage = pageValues.fallback;
+
+    renderHandler(res, fallbackPage);
+
+    return;
+  }
+
+  const languagePage = pageValues.langs[language];
   // check if key (language) is available
-  if (Object.prototype.hasOwnProperty.call(pageValues.langs, language)) {
-    if (pageValues.langs[language].file) {
-      res.sendFile(pageValues.langs[language].file, options);
-    }
-    // if key is not "file" make a redirect
-    else {
-      res.redirect(pageValues.langs[language].redirect);
-    }
+  if (languagePage) {
+    renderHandler(res, languagePage);
   }
-  // requested page language is not defined by host, use fallback page
-  else if (pageValues.fallback.file) {
-    res.sendFile(pageValues.fallback.file, options);
-  }
-  // if key is not "file" make a redirect
+  // requested page language is not defined by host, redirect to fallback page
   else {
-    res.redirect(pageValues.fallback.redirect);
+    res.redirect(pageValues.url);
   }
 });
 
