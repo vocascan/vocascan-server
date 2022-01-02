842bd23897d76ceaa77f9d350efd96bd89c087a3
luwol03 <60048565+luwol03@users.noreply.github.com>
2022-01-02 07:30:17 +0100

    fix cors if cors includes * (#73)

--- app/Middleware/SecurityMiddleware.js
+++ app/Middleware/SecurityMiddleware.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const config = require('../config/config');
 
 const CorsMiddleware = cors({
-  origin: config.server.cors,
+  origin: typeof config.server.cors !== 'boolean' && config.server.cors.includes('*') ? '*' : config.server.cors,
 });
 
 module.exports = [CorsMiddleware];
