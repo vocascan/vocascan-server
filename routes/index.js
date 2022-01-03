40a464ff7d004a80810acf2747e7e4f105c41f5d
Julian <noctera.dev@gmail.com>
2022-01-03 09:47:24 +0100

    Feature/legal templates (#72)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const APIRouter = require('./api');
+const PageRouter = require('./pages');
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.send('Hello World!');
 });
+
+router.use('/', PageRouter);
 
 router.use('/api', APIRouter);
 
